a37fd85480c7f1e203eb91e2161ad9e4961c6d5b
Alex <aleksey.bash@gmail.com>
2013-06-12 22:16:58 +0400

    PEW PEW PEW

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2995b6fb9213ba39d6d01707cc0799c5b5b5c2d138342a9d30ea1dfa0bb0206b64d7d3621c9f762cd9e97d7f160e9a6f5b0c87e18252cb2aec8069079e990ec3'
